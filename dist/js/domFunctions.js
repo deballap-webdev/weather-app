1dc24a9dfe26bcc928e3ee745f075a3e9a47d51a
deballap-webdev <debbieallap@gmail.com>
2026-08-01 13:22:12 +0100

    add displayError function with its helpers

--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -8,3 +8,17 @@ const animateButton = (element) => {
   element.nextElementSibling.classList.toggle("block");
   element.nextElementSibling.classList.toggle("none");
 };
+
+export const displayError = (headerMsg, srMsg) => {
+  updateWeatherLocationHeader(headerMsg);
+  updateScreenReaderConfirmation(srMsg);
+};
+
+const updateWeatherLocationHeader = (message) => {
+  const h2 = document.getElementById("currentForecast__location");
+  h2.textContent = message;
+};
+
+const updateScreenReaderConfirmation = (message) => {
+  document.getElementById("confirmation").textContent = message;
+};
