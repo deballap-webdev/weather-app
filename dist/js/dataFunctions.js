8b13f3090adb145160b4318e110983d7c25594f9
deballap-webdev <debbieallap@gmail.com>
2026-08-01 15:27:48 +0100

    add setLocationObject to update curerntLocation data

---
+++ dist/js/dataFunctions.js
@@ -0,0 +1,9 @@
+export const setLocationObject = (locationObject, coordsObj) => {
+  const { lon, lat, name, unit } = coordsObj;
+  locationObject.longitude = lon;
+  locationObject.latitude = lat;
+  locationObject.locationName = name;
+  if (unit) {
+    locationObject.currentUnit = unit;
+  }
+};
