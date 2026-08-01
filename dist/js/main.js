3e845bfce3934cadc1a8891613a644955b57ba10
deballap-webdev <debbieallap@gmail.com>
2026-08-01 17:34:46 +0100

    implement functionality for home (exempting fetch api)

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-import { setLocationObject } from "./dataFunctions.js";
+import { setLocationObject, getHomeLocation } from "./dataFunctions.js";
 import CurrentLocation from "./CurrentLocation.js";
 import { addSpinner, displayError } from "./domFunctions.js";
 
@@ -6,10 +6,15 @@ const currentLoc = new CurrentLocation();
 
 const initApp = () => {
   //add Listeners
-  const geoLocation = document.getElementById("getLocation");
-  geoLocation.addEventListener("click", getGeoWeather);
+  const geoButton = document.getElementById("getLocation");
+  geoButton.addEventListener("click", getGeoWeather);
+
+  const homeButton = document.getElementById("home");
+  homeButton.addEventListener("click", loadWeather);
+
   //set up
   //load default weather
+  loadWeather();
 };
 
 const getGeoWeather = (event) => {
@@ -38,8 +43,38 @@ const geoSuccess = (position) => {
   updateDataAndDisplay(currentLoc);
 };
 
-const updataDataAndDisplay = async (locationObj) => {
+const loadWeather = (event) => {
+  const savedLocation = getHomeLocation();
+  if (!savedLocation && !event) return getGeoWeather();
+  if (!savedLocation && event) {
+    displayError(
+      "No Home Location Saved",
+      "Sorry. Please save your home location first",
+    );
+  } else if (savedLocation && !event) {
+    displayHomeLocation(savedLocation);
+  } else if (savedLocation && event) {
+    const homeIcon = document.querySelector("fa-house-chimney");
+    addSpinner(homeIcon);
+  }
+};
+
+const updateDataAndDisplay = async (locationObj) => {
   // const weatherJson = await getWeatherFromCoords(locationObj);
   // if (weatherJson) updateDisplay(weatherJson, locationObj);
+};
+
+const displayHomeLocationWeather = (home) => {
+  if (typeof home === "string") {
+    const homeJson = JSON.parse(home);
+    const myCoordsObj = {
+      lat: homeJson.lat,
+      lon: homeJson.lon,
+      name: homeJson.name,
+      unit: homeJson.unit,
+    };
+    setLocationObject(currentLoc, myCoordsObj);
+    updateDataAndDisplay(currentLoc);
+  }
 };
 document.addEventListener("DOMContentLoaded", initApp());
